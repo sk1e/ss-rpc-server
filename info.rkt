000aac259ea08878ed938ea7c1da69079029f824
Sergey Petrov <sekk1e@yandex.ru>
2015-12-15 22:26:06 +0600

    fix info

--- info.rkt
+++ info.rkt
@@ -3,3 +3,7 @@
 
 (define collection "ss-rpc-server")
 (define scribblings '(("main.scrbl" ())))
+
+(define deps '("base" "srfi-lite-lib" "web-server-lib" ))
+
+(define build-deps '("scribble-lib" "racket-doc"))
